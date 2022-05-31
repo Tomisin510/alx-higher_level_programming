b712db5d939dacdf79149eec33d06a138b0344eb
Tomisin510 <danieltomisin97@gmail.com>
2022-05-31 08:23:51 -0700

    13. Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,71 +1,44 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-listint_t *create_node(int n);
+#include <unistd.h>
 
 /**
- * insert_node - inserts a node sorted in a linked list of ints
- * @head: double pointer to head of LL, needed for modification in edge
- * cases
- * @number: data for new node
+ * insert_node - inserts a number in an ordered linked list
+ * @head: double pointer to the linked list
+ * @number: number to insert in the new node
  *
- * Return: pointer to newly created node, NULL on failure
+ * Return: address of the new node, or NULL
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *cur_node = NULL, *new_node = NULL;
+	listint_t *current = *head;
+	listint_t *new = NULL;
+	listint_t *temp = NULL;
 
 	if (!head)
 		return (NULL);
-	else if (!(*head))
-	{
-		new_node = create_node(number);
-		*head = new_node;
-		return (new_node);
-	}
-	cur_node = *head;
-	while (cur_node)
-	{
-		/* need to insert at head */
-		if (cur_node->n >= number)
-		{
-			new_node = create_node(number);
-			new_node->next = cur_node;
-			*head = new_node;
-			return (new_node);
-		}
-		else if (cur_node->n <= number)
-		{
-			if (!cur_node->next || cur_node->next->n >= number)
-			{
-				new_node = create_node(number);
-				new_node->next = cur_node->next;
-				cur_node->next = new_node;
-				return (cur_node->next);
-			}
-		}
-		cur_node = cur_node->next;
-	}
-	return (NULL); /* failed */
-}
 
-
-/**
- * create_node - creates a new node for the LL
- * @n: data to insert into new node
- *
- * Return: pointer to newly allocated node
- */
-listint_t *create_node(int n)
-{
-	listint_t *ret = NULL;
-
-	ret = malloc(sizeof(listint_t));
-	if (!ret)
+	new = malloc(sizeof(listint_t));
+	if (!new)
 		return (NULL);
-	ret->next = NULL;
-	ret->n = n;
-	return (ret);
+	new->n = number;
+	new->next = NULL;
+
+	if (!*head || (*head)->n > number)
+	{
+		new->next = *head;
+		return (*head = new);
+	}
+	else
+	{
+		while (current && current->n < number)
+		{
+			temp = current;
+			current = current->next;
+		}
+		temp->next = new;
+		new->next = current;
+	}
+
+	return (new);
 }
