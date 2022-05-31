c2147650c41e19f6dd1329beb05646e5c19500a3
Tomisin510 <danieltomisin97@gmail.com>
2022-05-31 06:36:39 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-
-def print_low_alpha():
-    for c in 'abcdefghijklmnopqrstuvwxyz':
-        print('{}'.format(c), end='')
-if __name__ == '__main__':
-    print_low_alpha()
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
+        print("{}".format(i), end="")
