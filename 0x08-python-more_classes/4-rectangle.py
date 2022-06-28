9d1ff966e10d7328f3fd9128803582b42d9a238f
Tomisin510 <danieltomisin97@gmail.com>
2022-06-28 04:47:07 -0700

    4. Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,25 +1,17 @@
-
 #!/usr/bin/python3
-"""define rectangle class"""
+"""
+This is the "Rectangle"  module.
+This module provides a Rectangle class.
+"""
 
 
 class Rectangle:
-    """class rectangle"""
+    """A Rectangle class with attributes width and height, and
+    methods area, perimeter, print, str, and repr.
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-        if value < 0:
-            raise ValueError('height must be >= 0')
-        self.__height = value
 
     @property
     def width(self):
@@ -27,36 +19,39 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError("width must be >= 0")
         self.__width = value
 
-    def area(self):
-        """calculate the area of the rectangle"""
-        return self.__height * self.__width
+    @property
+    def height(self):
+        return self.__height
 
-    def perimeter(self):
-        """calculate the perimeter of the rectangle"""
-        if self.__height is 0 or self.__width is 0:
-            return 0
-        return self.__height * 2 + self.__width * 2
-
-    def __str__(self):
-        """create a string"""
-
-        string = ""
-        if self.__width is not 0 or self.__height is not 0:
-            for i in range(self.__height):
-                for y in range(self.__width):
-                    string += "#"
-                if i < self.__height - 1:
-                    string += '\n'
-        else:
-            string += '\n'
-        return string
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def __repr__(self):
-        """official string representation of the retangle"""
-        return 'Rectangle({:d}, {:d})'.format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __str__(self):
+        total = ""
+        for i in range(self.__height):
+            total += ("#" * self.__width)
+            if i is not self.__height - 1:
+                total += "\n"
+        return total
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
